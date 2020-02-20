3ebc286be5f7eff64c8cf11d43e6c26738578d70
chinaddse <45563412+chinaddse@users.noreply.github.com>
2020-02-20 16:02:24 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'lcmEMGX85pgtnCWX960_#*|',
+  'client_id' => '7a2758ce-74e0-426c-ad9c-bdd15fed753d',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAABeAFzDwllzTYGDLh_qYbH8jbmrfwegclkxwX0upxMHjw42swxJ8SVDea_9su-co7GaDaQJnDd9PlRNO3OWpss9ADZdIOcxCZL8Wo7Dtdz34QROaaeM91swGZTaDjaGSPSRBBjlY_SCMSaTBXqX85EY-WQ_zK48hK-4DoVK1uiSXRWQmBDzSVgDf31tZ15zSOFfjykwDdDehn4DunFah1bAmudHyva5bQZB_nIalXBg1yIecy85Y5hmHRuVd9QAyZmIEpNedNO8Elj5JrAsUoOPR9RRxI5aZJrt144R5U-udzq29CcVTQbXZo1nbRbWEBNxo7ulAF0HrwtAzo4TbPK_4ErKzkGWcwzFiVkdI1ACzchUngebDBMbcbDYUz9-fjFfujXm8umrVwmSDbEpLjSneZXM5IYhuuGhaljxJE9PbkZWWmJLf07YQ-ZiCEwPTUM6Z73dz569F1Ww_8KK7AVAiUwCpCGMmHml3iXTrhpqXwrtJ6VOFJAZ5yMNuFfDnIs6-P-z38qLUXBVlQ69EpaJDo0nKGzJb1nXt4mrLANvlmJ0Nsm15t1PRQ43UmOmUxpFy3n_GtWT_Y9dj0YlUzhzWU4TtdRJ6SY3vEfFF-u9eRljNHmPRb8SJDgn3Jr4i30nw7PZouoGr8vKv6jU-2L-frewfsbKhDEa0IjS-ka_5_UajK7sxttGlWRHn8BsBc-6n_dc1oSYZiVUT8z76GWs9wg_1L9YfW_Ej_xW7FDrmbXfnRsxUF7hC1sOfVhlO4ENhFfHWiZtU-IybODWeMQnxDiH5iokHtIy048EOEcvk6psUvuZXf9UDj0acIf3fG3JbVDNh6_NfM4AT-NftZ3nIAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
