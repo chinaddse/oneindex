86c82a7402c255120721bdc85bb317a8cb06926c
chinaddse <45563412+chinaddse@users.noreply.github.com>
2020-04-19 12:38:02 +0800

    Add files via upload

--- config/base.php
+++ config/base.php
@@ -1,12 +1,12 @@
 <?php return array (
-  'site_name' => 'chjina的网盘',
-  'password' => 'chjinas@10086',
+  'site_name' => '图床',
+  'password' => 'chjinas10086',
   'style' => 'nexmoe',
   'onedrive_root' => '/',
   'cache_type' => 'secache',
-  'cache_expire_time' => 1200,
+  'cache_expire_time' => 1800,
   'cache_refresh_time' => 600,
-  'root_path' => '',
+  'root_path' => '?',
   'show' => 
   array (
     'stream' => 
@@ -98,10 +98,10 @@
       4 => 'mp4',
     ),
   ),
-  'client_secret' => 'anAUDD2318[ekyteUVX8$~_',
-  'client_id' => 'bfbbd9e3-fcbb-4d6b-ae3a-a4c836215848',
+  'client_secret' => 'orTJKQ44|puazeCGL698]!}',
+  'client_id' => 'f9935e88-08c3-4741-9a77-ea55263f6fcc',
   'redirect_uri' => 'https://oneindex.github.io/',
-  'refresh_token' => 'OAQABAAAAAAAm-06blBE1TpVMil8KPQ416a70AsDQ51ZOxUCr8-SU-szpWP3N0jTlPc5e0vHrI8w1WH13rqwKSRUA0rAAp3hO7ojsBRAIsnJ9gT2PzK2gDzmy-Act7XIz6yFYqriBqglzqMmJ6oH83GxymwwzA0AeVbAqpIK_77__63SjsCyp76nvJWxmzQcjua5vw8tMkiarWbRxDkr84EjblggNtcwsiH1YKmE4ZKaAYrANvR1zH_RY7Wjsc5msWYvYPp2RDxJRWcdVAFZWAggEatTA1UxxZvEQMzpR3rCeasWirN5qlqhXYgQeCSRY2b0Yyb7t6APvNs5uVQlt7C-8FtVXBHBOg0fFY8USgZBIICIHL4-i1IiNCxTmf4MhS-Mo0amn6y2x32bukbaUiBUgxDqKcLnQdgejm3e5uh3hAmIWVI3J64iuiV2yxqZRFtwwA1IPYVMf37fewkoiZnjP4VRLRaPD2zelyD5yjJC1ZG-gbZ-4ZW7Y313Ifishm_Exe5uHZ0ZJ6hgWiqVCbuGna_X8Em96iEHFWI_Uw2q-s56CCDlw00EpggFKPMHJGbhSgpBSJmdk5tCR2SRFocabXjsH9wXNgIYoI0TbfnC_h1fTo3nsgKrDZSNITwSvSnc6yEp6NZZv_N8CegeJddfP1cuTmWbrCAgdUWyh-yYanyfBeBmm2gtn7A4Ak3tobKwyWi2dr_7ZrNeZhhcB08up09Xp34_9DHJug5p0Lb3-iDKIMaoQU6DAu7MMFq82Qyb7BqF6BKDucPs64gdejXQ2FpWmCBxWOHvUg0N25pt3v7BFXdBkrM1d3RT0KkPgMiyoEXT1bxe5LEOYoz-cJu0Ea4-GyvKAIAA',
+  'refresh_token' => 'MCb4zQL9p4LbnK5zMENhHworEGr2*FpdlVH6BwsUqmsRRe13MXYPF07hXlh3nq6Naa8Tn1h6dhA7x7izn8DZ9UCXBMjPhC41Y7CT!9ZWtY*nxW2NGf4PQLNX!UWNQ2dxNxS1Q!odP7l4Y3ixGjoFL8URCeTg6qXobTXxZL06vqY68Vo2934aC7sUGY7vZFoEMLzEXulkv1bdWlJ6xp8eC9!yYsxauGe5QYF0kBB9uh7lnf*EhNhDcoQMq2FYUUXpkT6t61f2UjWaLhLeePAs9biEK0jRaXJb9nMrXKUsmvg4O82Dx9H1*Ls45n*mfw6BfQ2rWAIcJ!4xwIuLdBkZCtL4Kig54SB3K6yh0msEbWpjpP1zNrufQSMdlbX6BtMHPI6kB!lk4G3zosDhyzNRR458$',
   'onedrive_hide' => '',
   'onedrive_hotlink' => '',
 );
